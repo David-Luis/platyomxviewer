3dd70f9f0b4fbf19c52da6978210f242890a5bb1
David Luis <david.luis.87@gmail.com>
2018-03-24 18:35:58 +0100

    fix parameters

--- OmxPlayer.py
+++ OmxPlayer.py
@@ -1,9 +1,15 @@
 import pexpect
 import platform
+import sys
 
-def play(self, file_path):
-    system = platform.system().lower()
-    if "windows" in system:
-        pexpect.spawn("vlc " + '"' + file_path)
-    else:
-        pexpect.spawn("omxplayer " + file_path)
+def play(file_path):
+
+    try:
+        system = platform.system().lower()
+        if "windows" in system:
+            pexpect.spawn("vlc " + '"' + file_path)
+        else:
+            pexpect.spawn("omxplayer " + file_path)
+    except:
+        print(sys.exc_info()[0])
+        print(sys.exc_info()[1])
