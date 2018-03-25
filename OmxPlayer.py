e2773e72e1415f1bd6608e70bb7b909f7680eeaa
David Luis Alonso <david.luis.87@gmail.com>
2018-03-25 17:07:57 +0200

    subtitles center :)

--- OmxPlayer.py
+++ OmxPlayer.py
@@ -24,7 +24,7 @@ class OmxPlayer():
                 self.process = pexpect.spawn("vlc " + + file_path)
             else:
                 print("SPAWN " + file_path)
-                self.process = pexpect.spawn('omxplayer "' + file_path + '"')
+                self.process = pexpect.spawn('omxplayer "' + file_path + '"' + ' --align center')
                 self.observer.on_play()
                 self.state = OmxPlayer.PLAYING
         except:
